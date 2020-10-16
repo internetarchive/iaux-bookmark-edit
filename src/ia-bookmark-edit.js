d170ec2a6276abeeee182fb4cd3ccfa30e78cda0
Shane Riley <shane@shaneriley.com>
2020-10-16 11:36:56 -0400

    Shortens action labels

--- src/ia-bookmark-edit.js
+++ src/ia-bookmark-edit.js
@@ -99,8 +99,8 @@ export class IABookmarkEdit extends LitElement {
             ${repeat(this.bookmarkColors, color => color.id, this.bookmarkColor.bind(this))}
           </ul>
           <div class="actions">
-            <input class="button" type="submit" value="Save changes" />
-            <button class="button" @click=${this.emitDeleteEvent}>Delete bookmark</button>
+            <input class="button" type="submit" value="Save" />
+            <button class="button" @click=${this.emitDeleteEvent}>Delete</button>
           </div>
         </fieldset>
       </form>
